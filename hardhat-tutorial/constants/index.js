ad58a21a0230af4a1d109e4f659192f26afd3cca
Marcus Torry <91373494+mtorry225@users.noreply.github.com>
2022-02-05 17:30:57 -0600

    crypto devs ico

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0xC36678aA7947760E544478B3839E8117b5e668da";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
